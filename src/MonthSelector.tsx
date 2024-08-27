5bf5d2a8bcbcac643a4566068ffe962c7dd2f564
shokkun47 <shoki1310@gmail.com>
2024-08-28 02:37:22 +0900

    addnewfiles

--- src/MonthSelector.tsx
+++ src/MonthSelector.tsx
@@ -3,8 +3,37 @@ import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers'
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3'
 import React from 'react'
 import { ja } from "date-fns/locale"
+import { addMonths } from 'date-fns'
 
-const MonthSelector = () => {
+interface MonthSelectorProps {
+  currentMonth: Date;
+  setCurrentMonth: React.Dispatch<React.SetStateAction<Date>>
+}
+
+const MonthSelector = ({ 
+  currentMonth,
+  setCurrentMonth,
+}: MonthSelectorProps) => {
+
+  const handleDateChange = (newDate: Date | null) => {
+    if (newDate) {
+      setCurrentMonth(newDate);
+    }
+  }
+
+  // 先月ボタンを押したときの処理
+  const handlePreviousMonth = () => {
+    const previousMonth = addMonths(currentMonth, -1);
+    setCurrentMonth(previousMonth);
+  }
+
+  // 次月ボタンを押したときの処理
+  const handleNextMonth = () => {
+    const NextMonth = addMonths(currentMonth, +1);
+    setCurrentMonth(NextMonth);
+  }
+
+
   return (
     <LocalizationProvider
       dateAdapter={AdapterDateFns}
@@ -15,10 +44,16 @@ const MonthSelector = () => {
     >
       <Box
         sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-        <Button color={"error"} variant="contained">
+        <Button 
+          onClick={handlePreviousMonth}
+          color={"error"} 
+          variant="contained"
+        >
           先月
         </Button>
         <DatePicker
+          onChange={handleDateChange}
+          value={currentMonth}
           label="年月を選択"
           sx={{ mx: 2, background: "white" }}
           views={["year", "month"]}
@@ -29,7 +64,11 @@ const MonthSelector = () => {
             },
           }}
         />
-        <Button color={"primary"} variant="contained">
+        <Button 
+          onClick={handleNextMonth}
+          color={"primary"} 
+          variant="contained"
+        >
           次月
         </Button>
       </Box>
--- src/pages/Report.tsx
+++ src/pages/Report.tsx
@@ -5,7 +5,15 @@ import CategoryChart from '../components/CategoryChart';
 import TransactionTable from '../components/TransactionTable';
 import BarChart from '../components/BarChart';
 
-function Report() {
+interface ReportProps {
+  currentMonth: Date;
+  setCurrentMonth: React.Dispatch<React.SetStateAction<Date>>
+}
+
+const  Report = ({ 
+  currentMonth, 
+  setCurrentMonth
+}: ReportProps) => {
   const commonPaperStyle = {
     height: { xs: "auto", md: "400px" },
     display: "flex",
@@ -14,18 +22,26 @@ function Report() {
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
-        <MonthSelector />
+        {/* 日付選択エリア */}
+        <MonthSelector
+          currentMonth={currentMonth} 
+          setCurrentMonth={setCurrentMonth}
+        />
       </Grid>
+
       <Grid item xs={12} md={4}>
         <Paper sx={commonPaperStyle}>
+          {/* 円グラフ */}
           <CategoryChart />
         </Paper>
       </Grid>
+
       <Grid item xs={12} md={8}>
         <Paper sx={commonPaperStyle}>
           <BarChart />
         </Paper>
       </Grid>
+
       <Grid item xs={12}>
         <TransactionTable />
       </Grid>
